9f43b9f5b7af8623e690e3d5115f9776a3e5fddd
Kshitij <m.krishna0513@gmail.com>
2026-02-05 19:54:35 +0530

    Updated jsp files

--- src/main/java/com/kk/JobApp/JobController.java
+++ src/main/java/com/kk/JobApp/JobController.java
@@ -4,9 +4,12 @@ import com.kk.JobApp.model.JobPost;
 import com.kk.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -28,5 +31,12 @@ public class JobController {
     public String handleForm(JobPost jobPost){
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewalljobs(Model m){
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
