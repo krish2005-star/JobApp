c5a2d4aceb71330965c06e7963520679489eb2e0
Kshitij <m.krishna0513@gmail.com>
2026-02-05 16:36:38 +0530

    Coded Job Controller file and created JobPost

---
+++ src/main/java/com/kk/JobApp/JobController.java
@@ -0,0 +1,26 @@
+package com.kk.JobApp;
+
+import com.kk.JobApp.model.JobPost;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @GetMapping({"/","home"})
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost){
+        return "success";
+    }
+}
